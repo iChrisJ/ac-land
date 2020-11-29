dfeda1da2d8f5d71bcb4950d1be6443999ecb46a
iChrisJ <chrisjiang@outlook.com>
2020-11-29 14:41:36 +0800

    fix: revert to original

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'AC Land',
+  title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -10,19 +10,19 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'AC LAND',
+      title: 'My Site',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
       },
       items: [
-        // {
-        //   to: 'docs/',
-        //   activeBasePath: 'docs',
-        //   label: 'Docs',
-        //   position: 'left',
-        // },
-        {to: 'blog', label: 'Blog', position: 'right'},
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -84,19 +84,17 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        // docs: {
-        //   sidebarPath: require.resolve('./sidebars.js'),
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/edit/master/website/',
-        // },
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
         blog: {
-          path: "./blog",
-          routeBasePath: "/",
           showReadingTime: true,
           // Please change this to your repo.
-          //editUrl:
-          //  'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
